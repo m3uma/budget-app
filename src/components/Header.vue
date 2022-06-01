b8b954d89aa5a1a2088346302b2910588fef5e02
Mateusz Bernacik <mat.bernacik@gmail.com>
2022-06-01 23:46:34 +0200

    add header component and refactor title

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <Title :title-text="title" @tick="e => msg = e" />
+    </header>
+</template>
+
+<script setup>
+import Title from '@/components/atoms/Title.vue';
+import { ref } from 'vue';
+
+const title = ref("Categories");
+let msg = ref('');
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    min-height: 6rem;
+    background: linear-gradient(90deg, #FF7F0A 0%, #FF9F40 100%);
+    padding-left: 2rem;
+    max-width: 100vw;
+}
+</style>
--- src/components/atoms/Title.vue
+++ src/components/atoms/Title.vue
@@ -1,30 +1,20 @@
+<script setup>
+const props = defineProps(['titleText'])
+const emit = defineEmits(['tick'])
+emit('tick', '')
+</script>
+
 <template>
     <h1>{{ titleText }}</h1>
 </template>
 
-<script setup>
-  const props = defineProps(['titleText'])
-  const emit  = defineEmits(['tick'])
-  emit('tick', '')
-</script>
-
 <style scoped lang="scss">
-    h1 {
-        position: absolute;
-        margin: 0;
-        width: 100%;
-        height: 105px;
-        left: 0px;
-        top: 0px;
-        background: linear-gradient(90deg, #FF7F0A 0%, #FF9F40 100%);
-        color: #FFFFFF;
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 600;
-        font-size: 48px;
-        line-height: 56px;
-        padding-left: 50px;
-        display: flex;
-        align-items: center;
-    }
+h1 {
+    color: #FFFFFF;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 48px;
+    margin: 0px;
+}
 </style>
