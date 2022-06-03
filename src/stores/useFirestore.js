6167ac834714cdc4a625a133c999fdf5bfca8b38
WojCzu <wojciech.czuma@gmail.com>
2022-06-03 17:25:30 +0200

    feat(pinia): add pinia config

---
+++ src/stores/useFirestore.js
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia';
+
+export const useFirestore = defineStore({
+  id: 'firestoreStore',
+  state: () => ({
+    // values
+  }),
+  getters: {
+    // get values from state
+  },
+  actions: {
+    // modify values
+  },
+});
