7b429f07c2471f188faee3827fbd3af154ca2598
WojCzu <wojciech.czuma@gmail.com>
2022-06-05 00:13:48 +0200

    feat(Login): add inputs and functions to login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,42 @@
 <template>
   <h1>Login view</h1>
+  <form @submit.prevent="handleSubmit">
+    <label for="email">Email:</label>
+    <input type="email" name="email" v-model="email" required />
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" v-model="password" required />
+
+    <button>Login</button>
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">{{ error }}</div>
+  </form>
 </template>
 
-<script setup></script>
+<script setup>
+import { login } from '@/composable/firesbase';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const email = ref('');
+const password = ref('');
+
+const isLoading = ref(false);
+const error = ref(null);
+
+const handleSubmit = async () => {
+  isLoading.value = true;
+  error.value = null;
+  try {
+    await login({ email: email.value, password: password.value });
+    router.push('/');
+  } catch (e) {
+    error.value = e.message;
+    isLoading.value = false;
+  }
+};
+</script>
 
 <style scoped></style>
