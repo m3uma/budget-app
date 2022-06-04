ad66c7a1bea29f17fa7df09ce7d66d9c633818c8
WojCzu <wojciech.czuma@gmail.com>
2022-06-05 00:03:26 +0200

    feature(SignUp): add inputs and functions to signup view

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,7 +1,56 @@
 <template>
   <h1>Sign up view</h1>
+  <form @submit.prevent="handleSubmit">
+    <label for="email">Email:</label>
+    <input type="email" name="email" v-model="email" required />
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" v-model="password" required />
+
+    <label for="confirm-password">Confirm password:</label>
+    <input type="password" name="confirm-password" v-model="confirmPassword" required />
+
+    <button>Sign up</button>
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">{{ error }}</div>
+  </form>
 </template>
 
-<script setup></script>
+<script setup>
+import { signUp } from '@/composable/firesbase';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const email = ref('');
+const password = ref('');
+const confirmPassword = ref('');
+
+const isLoading = ref(false);
+const error = ref(null);
+
+const handleSubmit = async () => {
+  if (password.value.length < 6) {
+    error.value = 'password must be at least 6 characters long';
+    return;
+  }
+  if (password.value !== confirmPassword.value) {
+    error.value = 'password and confirmation password must be the same';
+    return;
+  }
+
+  isLoading.value = true;
+  error.value = null;
+
+  try {
+    await signUp({ email: email.value, password: password.value });
+    router.push('/');
+  } catch (e) {
+    error.value = e.message;
+    isLoading.value = false;
+  }
+};
+</script>
 
 <style scoped></style>
