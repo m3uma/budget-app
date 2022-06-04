f71c57ad48af6e12d9e3ec550b6164346666d875
WojCzu <wojciech.czuma@gmail.com>
2022-06-04 18:28:51 +0200

    fix(firebase): change env imports to proper ones (from process.env to import.meta.env)

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_FIRESTORE_KEY,
-  authDomain: process.env.VUE_APP_FIRESTORE_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_FIRESTORE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIRESTORE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIRESTORE_SENDER_ID,
-  appId: process.env.VUE_APP_FIRESTORE_APP_ID,
+  apiKey: import.meta.env.VITE_FIRESTORE_KEY,
+  authDomain: import.meta.env.VITE_FIRESTORE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIRESTORE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIRESTORE_SENDER_ID,
+  appId: import.meta.env.VITE_FIRESTORE_APP_ID,
 };
 
 initializeApp(firebaseConfig);
