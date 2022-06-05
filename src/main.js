9e28c663d2f9a95a33da6853e45cb18192ba24ab
WojCzu <wojciech.czuma@gmail.com>
2022-06-05 14:11:11 +0200

    refactor(pinia): create pinia in stores index

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
+import { pinia } from './stores';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).use(createPinia()).mount('#app');
+createApp(App).use(router).use(pinia).mount('#app');
