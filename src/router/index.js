fb7d190b9260054049c00519988505f3b68d6633
WojCzu <wojciech.czuma@gmail.com>
2022-06-05 13:54:34 +0200

    feat(router): add route guard before enter

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { Categories, Expenses, Login, SignUp, ErrorPage } from '@/views';
+import { useFirestore } from '@/stores/useFirestore';
+
+const AUTH_PATHS = ['Categories', 'Expenses', 'NotFound'];
+const UNAUTH_PATHS = ['Login', 'SignUp'];
+
+function checkIfUserIsAuthenicated(to, _, next) {
+  const piniaFire = useFirestore();
+  if (!piniaFire.user && !UNAUTH_PATHS.includes(to.name)) return next({ name: 'Login' });
+  else if (piniaFire.user && !AUTH_PATHS.includes(to.name)) return next({ name: 'Categories' });
+  else next();
+}
 
 const routes = [
   {
     path: '/categories',
     name: 'Categories',
     component: Categories,
+    beforeEnter: [checkIfUserIsAuthenicated],
   },
   {
     path: '/expenses/:category?',
     name: 'Expenses',
     component: Expenses,
+    beforeEnter: [checkIfUserIsAuthenicated],
     props: true,
   },
   {
     path: '/login',
     name: 'Login',
     component: Login,
+    beforeEnter: [checkIfUserIsAuthenicated],
   },
   {
     path: '/sign-up',
     name: 'SignUp',
     component: SignUp,
+    beforeEnter: [checkIfUserIsAuthenicated],
   },
   {
     path: '/',
@@ -31,6 +46,7 @@ const routes = [
     path: '/:catchAll(.*)',
     name: 'NotFound',
     component: ErrorPage,
+    beforeEnter: [checkIfUserIsAuthenicated],
   },
 ];
 
